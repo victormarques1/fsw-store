753ede4a6f53738d7eff15a75bd8b8010408fa80
Victor Marques <marques.vct@hotmail.com>
2023-10-18 19:20:58 -0300

    feat: add ProductsItem and ProductListComponent

---
+++ src/components/ui/product-item.tsx
@@ -0,0 +1,58 @@
+import { ProductWithTotalPrice } from "@/helpers/product";
+import { Product } from "@prisma/client";
+import { ArrowDown, ArrowDownIcon } from "lucide-react";
+import Image from "next/image";
+import { Badge } from "./badge";
+
+interface ProductItemProps {
+  product: ProductWithTotalPrice;
+}
+
+const ProductItem = ({ product }: ProductItemProps) => {
+  return (
+    <div className="flex max-w-[156px] flex-col gap-4">
+      <div className="relative flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
+        <Image
+          src={product.imageUrls[0]}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto max-h-[80%] w-auto max-w-[70%]"
+          style={{ objectFit: "contain" }}
+          alt={product.name}
+        />
+
+        {product.discountPercent > 0 && (
+          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+            <ArrowDown size={14}/> {product.discountPercent}%
+          </Badge>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+          {product.name}
+        </p>
+
+        <div className="flex items-center justify-center gap-2">
+          {product.discountPercent > 0 ? (
+            <>
+              <p className="font-semibold">
+                R$ {product.totalPrice.toFixed(2)}
+              </p>
+              <p className="text-xs line-through opacity-75">
+                R$ {Number(product.basePrice).toFixed(2)}
+              </p>
+            </>
+          ) : (
+            <p className="text-sm font-semibold">
+              R$ {product.basePrice.toFixed(2)}
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
