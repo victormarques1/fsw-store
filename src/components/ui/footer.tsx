9e2deda4026a28b25a96bcc095c4c10246425b25
Victor Marques <marques.vct@hotmail.com>
2023-10-30 15:37:47 -0300

    feat: add Footer components

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div className="bg-accent px-8 py-4 text-[10px] opacity-75">© 2023 Copyright <span className="font-semibold">FSW Store</span></div>;
+};
+
+export default Footer;
